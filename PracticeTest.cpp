46be096b59bf6301a3640ff12f250c3429acdb7a
mastenl <31227206+mastenl@users.noreply.github.com>
2020-04-26 19:24:41 -0700

    Add third unit test

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -27,3 +27,10 @@ TEST(PracticeTest, is_not_a_palindrome)
     bool actual = obj.isPalindrome("Ba");
     ASSERT_FALSE(actual);
 }
+
+TEST(PracticeTest, is_wierd_acpita_name_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("hannaH");
+    ASSERT_TRUE(actual);
+}
